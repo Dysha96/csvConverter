a1ea478daa4bec1e1526f2a1f5fbba3f1318e431
Dysha96 <Dyha96@inbox.ru>
2018-08-06 21:04:03 +0700

    add temporary files for the test

--- index.php
+++ index.php
@@ -19,3 +19,25 @@ if (!$parameters->getInputPath() || !$parameters->getConfigPath() || !$parameter
     echo $parametersParser->help();
     exit;
 }
+
+$delimiter = $parameters->getDelimiter();
+
+//$inCsv = array_map(function ($value) use ($delimiter) {
+//    return str_getcsv($value, $delimiter, "\"\"", "\\");
+//}, file($parameters->getInputPath()));
+//print_r($inCsv);
+
+$inCsv = new SplFileObject($parameters->getInputPath());
+$paramsInFile = $inCsv->getCsvControl();
+//$inCsv->setFlags(SplFileObject::READ_CSV);
+$file = new SplFileObject($parameters->getOutputPath(), 'w');
+
+while (!$inCsv->eof()) {
+    $file->fputcsv(array_map(function ($value) {
+            return $value;
+        }, $inCsv->fgetcsv())
+        , $delimiter, $paramsInFile[1], $paramsInFile[2]);
+}
+
+print_r($inCsv->getCsvControl());
+$encoding = mb_detect_encoding(file_get_contents($parameters->getInputPath()), 'UTF-8');
