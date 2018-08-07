0a8bc4b85d01cd4ac3237357e8e6482240c15301
Dysha96 <Dyha96@inbox.ru>
2018-08-07 17:21:32 +0700

    added new features and refactoring

--- index.php
+++ index.php
@@ -1,43 +1,62 @@
 <?php
 require_once "vendor/autoload.php";
 require_once "service/ParametersParser.php";
+require_once "service/processingCsv.php";
 
 use service\parametersParser;
 
 $parametersParser = new parametersParser();
 $parameters = $parametersParser->getParameters();
 
-
 // show help and quit
 if ($parameters->isHelp()) {
     echo $parametersParser->help();
     exit;
 }
-
+// show help and quit
 if (!$parameters->getInputPath() || !$parameters->getConfigPath() || !$parameters->getOutputPath()) {
     echo "Insufficient data " . PHP_EOL;
     echo $parametersParser->help();
     exit;
 }
 
-$delimiter = $parameters->getDelimiter();
-
-//$inCsv = array_map(function ($value) use ($delimiter) {
-//    return str_getcsv($value, $delimiter, "\"\"", "\\");
-//}, file($parameters->getInputPath()));
-//print_r($inCsv);
-
-$inCsv = new SplFileObject($parameters->getInputPath());
-$paramsInFile = $inCsv->getCsvControl();
-//$inCsv->setFlags(SplFileObject::READ_CSV);
-$file = new SplFileObject($parameters->getOutputPath(), 'w');
-
-while (!$inCsv->eof()) {
-    $file->fputcsv(array_map(function ($value) {
-            return $value;
-        }, $inCsv->fgetcsv())
-        , $delimiter, $paramsInFile[1], $paramsInFile[2]);
+$encoding = mb_detect_encoding(file_get_contents($parameters->getInputPath()));
+// show help and quit
+if ($encoding != 'UTF-8' && $encoding != 'cp1251') {
+    echo "Incorrect encoding " . PHP_EOL;
+    echo $parametersParser->help();
+    exit;
 }
 
-print_r($inCsv->getCsvControl());
-$encoding = mb_detect_encoding(file_get_contents($parameters->getInputPath()), 'UTF-8');
+$inCsv = new SplFileObject($parameters->getInputPath());
+$inCsv->setFlags(SplFileObject::READ_CSV);
+$delimiter = $parameters->getDelimiter();
+$arrayConfig = require_once $parameters->getConfigPath();
+
+if ($parameters->isStrict() && $inCsv->valid()) {
+    $firstRowData = $inCsv->fgetcsv($delimiter);
+    $latestIndexRowData = count($firstRowData);
+    $latestIndexArrayConfig = max(array_keys($arrayConfig)) + 1;
+
+    if ($latestIndexArrayConfig > $latestIndexRowData) {
+        echo "The number of columns of the input file is less than in the config" . PHP_EOL;
+        exit;
+    }
+    $inCsv->rewind();
+}
+
+$paramsInFile = $inCsv->getCsvControl();
+$enclosure = $paramsInFile[1];
+$escape = $paramsInFile[2];
+$faker = Faker\Factory::create();
+$outCsv = new SplFileObject($parameters->getOutputPath(), 'w');
+iconv_set_encoding('output_encoding', $encoding);
+
+while ($inCsv->valid()) {
+    if ($inCsv->key() == 0 && $parameters->isSkipFirst()) {
+        $processedRowData = $inCsv->fgetcsv($delimiter);
+    } else {
+        $processedRowData = $process($inCsv->fgetcsv($delimiter), $arrayConfig, $faker);
+    }
+    $record($outCsv, $processedRowData, $delimiter, $enclosure, $escape);
+}
